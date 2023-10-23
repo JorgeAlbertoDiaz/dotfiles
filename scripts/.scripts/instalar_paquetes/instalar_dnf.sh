b27a7fea2ca126958b6dd86f4c6a74045d3fff2d
Jorge Alberto Diaz <Jorge.Alberto.Diaz@hotmail.com.ar>
2023-10-22 21:16:27 -0300

    feat: agrego script para instalar listado de paquetes en el sistema

---
+++ scripts/.scripts/instalar_paquetes/instalar_dnf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Especifica el nombre del archivo de lista de paquetes
+paquetes="paquetes_dnf.txt"
+
+# Verifica si el archivo de listado de paquetes existe
+if [ -f "$paquetes" ]; then
+  # Actualiza el listado de paquetes antes de intentar instalar los paquetes
+  sudo dnf update -y
+  
+  # Agregar atim/lazygit para poder instalarlo
+  sudo dnf copr enable atim/lazygit
+
+  # Utiliza el comando dnf para instalar paquetes de acuerdo al contenido del archivo
+  sudo dnf install -y $(cat "$paquetes")
+  # Verifica el código de salida de dnf para comprobar si la instalación fue exitosa
+  if [ $? -eq 0 ]; then
+    echo "Instalación exitosa."
+  else
+    echo "Error durante la instalación."
+  fi
+else
+  echo "El archivo '$paquetes' no se encuentra en el directorio actual."
+fi
